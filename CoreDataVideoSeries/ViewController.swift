47629b8e3a8be778e2ede56412ffe385d402ac8f
Chris Chadillon <chris@chadillon.com>
2017-02-19 16:50:44 -0500

    Added new VC with Objects Segue to the new VC

---
+++ CoreDataVideoSeries/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataVideoSeries
+//
+//  Created by Chris Chadillon on 2017-02-19.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
